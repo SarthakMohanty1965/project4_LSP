9adb7b68f3b63156abb2db47e92082a6665c135e
Sarthak Mohanty <mohantysarthak781@gmail.com>
2025-11-09 15:38:33 +0530

    Implement file upload in client.py
    Added file upload functionality to the client.

--- assignment4_LSP/client.py
+++ assignment4_LSP/client.py
@@ -1,14 +1,27 @@
-#  ______________________  ***** Day_1 *****    ______________________
 
 import socket
+import os
+
+filename = input("Enter filename to upload: ")
+if not os.path.exists(filename):
+    print("File not found.")
+    exit()
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 8888))
+client_socket.connect(('localhost', 5002))  
 
-client_socket.send("Hello from client!".encode())
 
-data = client_socket.recv(1024).decode()
-print("Server says:", data)
+client_socket.send(os.path.basename(filename).encode())
+status = client_socket.recv(1024).decode()
+if status != "OK":
+    print("Server error:", status)
+    client_socket.close()
+    exit()
 
+
+with open(filename, "rb") as f:
+    while chunk := f.read(1024):
+        client_socket.send(chunk)
+
+print(f"File '{filename}' uploaded successfully.")
 client_socket.close()
-
