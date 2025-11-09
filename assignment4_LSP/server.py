18eb0af7fd0193899448603984f16acb0b0710d2
Sarthak Mohanty <mohantysarthak781@gmail.com>
2025-11-09 15:38:00 +0530

    Implement file upload handling in server
    Added functionality to receive and save uploaded files.

--- assignment4_LSP/server.py
+++ assignment4_LSP/server.py
@@ -1,22 +1,33 @@
-# ______________________  ***** Day_2 *****    ______________________
 import socket
 import os
 
-def list_files():
-    files = os.listdir("shared")
-    return "\n".join(files)
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+def receive_file(conn, filename):
+    filepath = os.path.join(UPLOAD_DIR, filename)
+    with open(filepath, "wb") as f:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            f.write(data)
+    print(f"Received file: {filename}")
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost',8888))
+server_socket.bind(('localhost', 5002))  
 server_socket.listen(1)
+print("Server ready to receive uploads...")
 
 conn, addr = server_socket.accept()
 print(f"Connected by {addr}")
 
-#data = conn.recv(1024).decode()
-#print("Client says:", data)
 
-conn.send("Hello from server!".encode())
+filename = conn.recv(1024).decode()
+conn.send("OK".encode())  # Acknowledgeing
+
+
+receive_file(conn, filename)
 
 conn.close()
-server_socket.close()  c 206
+server_socket.close()
